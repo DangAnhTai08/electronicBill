6a58c4718a4f80cc4f7028172bf2e237ce8fb2d6
Tai Chef <taidang@tamdongtam.vn>
2021-05-04 19:18:21 +0700

    create project

---
+++ src/test/java/electronic/bill/BillApplicationTests.java
@@ -0,0 +1,13 @@
+package electronic.bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
